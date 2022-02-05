ea5dc025e5b0eb79e4878ceac219c83177d534af
Pavel <76093403+pavelkrasulin@users.noreply.github.com>
2022-02-05 16:22:05 +0400

    Исправляет методы для Store

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Map<String, Product> store = new HashMap<>();
+        Map<String, Product> storageOfGoods = new HashMap<>();
 
-        store.put("M20215890N2", new Product("Milk"));
-        store.put("M20215890N3", new Product("Bread"));
-        store.put("M20215890N4", new Product("Oil"));
-        store.put("M20215890N5", new Product("Water"));
-        store.put("M20215890N6", new Product("Eggs"));
-        store.put("M20215890N7", new Product("Cheese"));
+        storageOfGoods.put("M20215890N2", new Product("Milk"));
+        storageOfGoods.put("M20215890N3", new Product("Bread"));
+        storageOfGoods.put("M20215890N4", new Product("Oil"));
+        storageOfGoods.put("M20215890N5", new Product("Water"));
+        storageOfGoods.put("M20215890N6", new Product("Eggs"));
+        storageOfGoods.put("M20215890N7", new Product("Cheese"));
 
-        Store.printMap(store);
+        Store store = new Store();
+        store.printMap(storageOfGoods);
 
-        store.remove("M20215890N6");
+        storageOfGoods.remove("M20215890N6");
 
-        Set<Map.Entry<String, Product>> entrySet = store.entrySet();
+        Set<Map.Entry<String, Product>> entrySet = storageOfGoods.entrySet();
         List<Map.Entry<String, Product>> list = new ArrayList<>(entrySet);
 
-        Store.sortMapByValue(list);
+        store.sortMapByValue(list);
 
-        Store.printSortedMap(list);
+        store.printSortedMap(list);
     }
 }
--- src/Store.java
+++ src/Store.java
@@ -4,17 +4,17 @@ import java.util.Map;
 
 public class Store {
 
-    public static void printMap(Map<String, Product> store) {
+    public void printMap(Map<String, Product> storageOfGoods) {
         System.out.println("Текущий список товаров:");
-        store.forEach((key, value) -> System.out.printf("Артикул: %s Товар: %s \n", key, value.getName()));
-        System.out.println("Итого: " + store.size());
+        storageOfGoods.forEach((key, value) -> System.out.printf("Артикул: %s Товар: %s \n", key, value.getName()));
+        System.out.println("Итого: " + storageOfGoods.size());
     }
 
-    public static void sortMapByValue(List<Map.Entry<String, Product>> list) {
+    public void sortMapByValue(List<Map.Entry<String, Product>> list) {
         list.sort(Comparator.comparing(o -> o.getValue().getName()));
     }
 
-    public static void printSortedMap(List<Map.Entry<String, Product>> list) {
+    public void printSortedMap(List<Map.Entry<String, Product>> list) {
         System.out.println("\nОстаток товаров за прошедший месяц:");
         list.forEach(s-> System.out.println("Артикул: " + s.getKey() + " Товар: " + s.getValue().getName()));
         System.out.println("Итого: " + list.size());
